79f071e745cf79d509518e2541e76cdc07ad78df
lmk123 <me@limingkai.cn>
2017-09-20 18:01:48 +0800

    转换为百分百时始终去掉末尾的零

--- libs/percentage.js
+++ libs/percentage.js
@@ -4,15 +4,11 @@ import isNumber from './isNumber'
  * 将一个小数转换为百分比
  * @param {number|string} number
  * @param {number} [fixed=2]
- * @param [noZero] - 是否去掉末尾的 0
  * @return {string}
  */
-export default function (number, fixed = 2, noZero) {
+export default function (number, fixed = 2) {
   number = Number(number)
   if (!isNumber(number)) return ''
   let numberString = (number * 100).toFixed(fixed)
-  if (noZero) {
-    numberString = parseFloat(numberString)
-  }
-  return numberString + '%'
+  return parseFloat(numberString) + '%'
 }
--- test/percentage-spec.js
+++ test/percentage-spec.js
@@ -3,9 +3,9 @@ const { percentage } = require('../dist/nosh.common')
 describe('percentage 函数', function () {
   it('能正常运行', function () {
     expect(percentage('not a number')).toBe('')
-    expect(percentage('1')).toBe('100.00%')
-    expect(percentage(-0.1)).toBe('-10.00%')
-    expect(percentage(0.01, 4)).toBe('1.0000%')
+    expect(percentage('1')).toBe('100%')
+    expect(percentage(-0.1)).toBe('-10%')
+    expect(percentage(0.01, 4)).toBe('1%')
     expect(percentage(0.01, 4, true)).toBe('1%')
     expect(percentage('0.13324', 7, true)).toBe('13.324%')
   })
