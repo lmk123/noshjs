0aa8b23804dfcc2428bb13ff64851b43cdcadd6f
芊野 <free.nan.sky@gmail.com>
2017-10-17 12:22:25 +0800

    Add test

--- test/thousands-spec.js
+++ test/thousands-spec.js
@@ -2,7 +2,7 @@ const { thousands } = require('../dist/nosh.common')
 
 describe('thousands 函数', function () {
   it('能正常运行', function () {
-    expect(thousands('not a number')).toBe('')
+    expect(thousands('not a number')).toBe('not a number')
     expect(thousands(1000)).toBe('1,000')
     expect(thousands(-1000)).toBe('-1,000')
   })
