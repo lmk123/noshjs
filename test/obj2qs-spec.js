ea3a8c6ba5f36039c5eb5c958d4a5de54bff337d
Leo Deng <myst.dg@gmail.com>
2018-10-10 11:35:11 +0800

    fix(obj2qs-spec): update test cases

--- test/obj2qs-spec.js
+++ test/obj2qs-spec.js
@@ -1,7 +1,13 @@
 const { obj2qs } = require('../dist/nosh.common')
 
 describe('obj2qs 函数', function () {
-  it('能正常运行', function () {
+  it('参数拼接', function () {
     expect(obj2qs({ a: 'a', b: 'b' })).toBe('a=a&b=b')
+  })
+  it('参数拼接加 ? 前缀', function () {
+    expect(obj2qs({ apple: 'jack', rainbow: 'dash' }, '?')).toBe('?apple=jack&rainbow=dash')
+  })
+  it('参数拼接加 & 前缀', function () {
+    expect(obj2qs({ twilight: 'Sparkle', pinkie: 'pie' }, '&')).toBe('&twilight=sparkle&pinkie=pie')
   })
 })
